c73cd2984edf995de0d44e2a0db2bed169eabcab
corradopezzato <c.pezzato@tudelft.nl>
2022-06-23 15:51:05 +0200

    updated example

--- examples/example_adaptive_act_sel.py
+++ examples/example_adaptive_act_sel.py
@@ -16,11 +16,11 @@ mdp_isVisible = state_action_templates.MDPIsVisible()
 
 # Agent with following states [isAt, isHolding, isReachable, isPlacedAt, isVisible]
 ai_agent_task = [ai_agent.AiAgent(mdp_isAt), ai_agent.AiAgent(mdp_isHolding), ai_agent.AiAgent(mdp_isReachable), ai_agent.AiAgent(mdp_isPlacedAt), ai_agent.AiAgent(mdp_isVisible)]
+ai_agent_task[3].set_preferences(np.array([[1.], [0.]]))
 
 # Loop for the execution of the task, ideally this will be given by the tick of a BT
-for i in range(20):
+for i in range(25):
     # Set preference for a particular state to be achieved. Follow indexing in ai_agent_task [isAt, isHolding, isReachable, isPlacedAt, isVisible]
-    ai_agent_task[3].set_preferences(np.array([[1.], [0.]]))
     # Get the parameters of the object to be holding to define a suitable observation
     # most likely from the parameter server (TODO add the parameters from the BT)
 
@@ -34,5 +34,7 @@ for i in range(20):
         obs = ['null', 1, 0, 1, 0]    
     if i>= 15 and i < 20:
         obs = ['null', 0, 0, 1, 0]  
+    if i>= 20:
+        obs = ['null', 1, 0, 0, 0]  
     outcome, curr_acti = adaptive_action_selection.adapt_act_sel(ai_agent_task, obs)
-    #print(outcome)
+    print(outcome)
