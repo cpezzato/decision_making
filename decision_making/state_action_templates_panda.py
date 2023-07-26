53608b0a26386afc9078ceb4a92f2b2d29a359d9
cpezzato <cpezzato95@gmail.com>
2023-07-26 23:45:01 +0200

    adds pick and place example

---
+++ decision_making/state_action_templates_panda.py
@@ -0,0 +1,146 @@
+import numpy as np
+
+class MDPIsAtPlaceLoc:
+    def __init__(self): 
+        self.state_name = 'isAtPlaceLoc'                               
+        self.state_names = ['at_place_loc', 'not_place_loc']          
+        self.action_names = ['idle', 'move_to_place_loc']                 
+
+        self.V = np.array([0, 1])          
+        self.B = np.zeros((2, 2, 2))       
+
+        # Transition matrices
+        # ----------------------------------------------------------
+        self.B[:, :, 0] = np.eye(2)  # Idle action
+        self.B[:, :, 1] = np.array([[1, 1],         # move_to
+                                    [0, 0]])
+
+        # Preconditions of the actions above
+        # ----------------------------------------------------------
+        self.preconditions = [['none'], ['holding_obj']]    # No preconditions needed for Idle and move_to                       
+
+        # Likelihood matrix matrices
+        # ----------------------------------------------------------
+        self.A = np.eye(2)  # Identity mapping
+        # Prior preferences, initially set to zero, so no preference
+        # -----------------------------------------------------------
+        self.C = np.array([[0.], [0.]])
+        # Belief about initial state, D
+        # -----------------------------------------------------------
+        self.D = np.array([[0.5], [0.5]])
+
+        # Preference about actions, idle is slightly preferred
+        # -----------------------------------------------------------
+        self.E = np.array([[1.01], [1]])
+        # Learning rate for initial state update
+        # -----------------------------------------------------------
+        self.kappa_d = 1
+
+class MDPIsReachable:
+    def __init__(self): 
+        self.state_name = 'isReachable'                            
+        self.state_names = ['reachable', 'not_reachable']                
+        self.action_names = ['idle', 'move_to_reach']    
+
+        self.V = np.array([0, 1])  # Allowable policies, it indicates policies of depth 1
+        self.B = np.zeros((2, 2, 2))  # Allowable actions initiation
+        # Transition matrices
+        # ----------------------------------------------------------
+        self.B[:, :, 0] = np.eye(2)             # Idle action
+        self.B[:, :, 1] = np.array([[1, 1],     # move_to_reach
+                                    [0, 0]])
+
+        # Preconditions of the actions above
+        # ----------------------------------------------------------
+        self.preconditions = [['none'], ['none']]    # No preconditions needed for Idle and move_to_reach                       
+           
+
+        # Likelihood matrix matrices
+        # ----------------------------------------------------------
+        self.A = np.eye(2)  # Identity mapping
+        # Prior preferences, initially set to zero, so no preference
+        # -----------------------------------------------------------
+        self.C = np.array([[0.], [0.]])
+        # Belief about initial state, D
+        # -----------------------------------------------------------
+        self.D = np.array([[0.5], [0.5]])
+
+        # Preference about actions, idle is slightly preferred
+        # -----------------------------------------------------------
+        self.E = np.array([[1.01], [1]])
+        # Learning rate for initial state update
+        # -----------------------------------------------------------
+        self.kappa_d = 1
+
+
+class MDPIsHolding:
+    def __init__(self):
+        self.state_name = 'isHolding'                            
+        self.state_names = ['holding_obj', 'not_holding_obj']               
+        self.action_names = ['idle', 'pick']  
+
+        self.V = np.array([0, 1])  
+        self.B = np.zeros((2, 2, 2))
+        
+        # Transition matrices
+        # ----------------------------------------------------------
+        self.B[:, :, 0] = np.eye(2)  # Idle action
+        self.B[:, :, 1] = np.array([[1, 1],  # PickRight action
+                                    [0, 0]])
+        # Preconditions of the actions above
+        self.preconditions = [['none'], ['reachable'], ['none']]
+
+        # Likelihood matrix matrices
+        # ----------------------------------------------------------
+        self.A = np.eye(2)  # Identity mapping
+        # Prior preferences, initially set to zero, so no preference
+        # -----------------------------------------------------------
+        self.C = np.array([[0.], [0.]])
+        # Belief about initial state, D
+        # -----------------------------------------------------------
+        self.D = np.array([[0.5], [0.5]])
+        
+        # Preference about actions, idle is slightly preferred
+        # -----------------------------------------------------------
+        self.E = np.array([[1.01], [1]])
+        # Learning rate for initial state update
+        # -----------------------------------------------------------
+        self.kappa_d = 1
+
+class MDPIsPlacedOn:
+    def __init__(self):
+        self.state_name = 'isOnTop'                                                  
+        self.state_names = ['placed_on', 'not_placed_on']               
+        self.action_names = ['idle', 'place']   
+
+        self.V = np.array([0, 1])       
+        self.B = np.zeros((2, 2, 2))  
+        
+        # Transition matrices
+        # ----------------------------------------------------------
+        self.B[:, :, 0] = np.eye(2)             # Idle action
+        self.B[:, :, 1] = np.array([[1, 1],     # Action Place_on
+                                    [0, 0]])
+        # Preconditions of the actions above
+        # ----------------------------------------------------------
+        self.preconditions = [['none'], ['holding_obj', 'at_place_loc']]    # [idle precondition], [place_in_backet_obj]                  
+
+
+        # Likelihood matrix matrices
+        # ----------------------------------------------------------
+        self.A = np.eye(2)  # Identity mapping
+        # Prior preferences, initially set to zero, so no preference
+        # -----------------------------------------------------------
+        self.C = np.array([[0.], [0.]])
+        # Belief about initial state, D
+        # -----------------------------------------------------------
+        self.D = np.array([[0.5], [0.5]])
+        # Initial guess about the states d, all equally possible, this is updated over time
+        # -----------------------------------------------------------
+        self.d = np.array([[0.5], [0.5]])
+        # Preference about actions, idle is slightly preferred
+        # -----------------------------------------------------------
+        self.E = np.array([[1.01], [1]])
+        # Learning rate for initial state update
+        # -----------------------------------------------------------
+        self.kappa_d = 1
---
+++ examples/example_panda_pick_place.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+# Simple example to create an AI agent for pick and place
+
+import numpy as np
+from decision_making import ai_agent, state_action_templates_panda, adaptive_action_selection
+## Initialization
+# ----------------- 
+mdp_isAtPlaceLoc = state_action_templates_panda.MDPIsAtPlaceLoc() 
+mdp_isHolding = state_action_templates_panda.MDPIsHolding() 
+mdp_isPlacedOn = state_action_templates_panda.MDPIsPlacedOn() 
+mdp_isReachable = state_action_templates_panda.MDPIsReachable() 
+
+# Agent with following states [isAtPlaceLoc, isHolding, isReachable, isPlacedOn], see templates
+ai_agent_task = [ai_agent.AiAgent(mdp_isAtPlaceLoc), ai_agent.AiAgent(mdp_isHolding), ai_agent.AiAgent(mdp_isReachable), ai_agent.AiAgent(mdp_isPlacedOn)]
+# Define the task for an agent by setting the preferences
+ai_agent_task[3].set_preferences(np.array([[1.], [0.]]))
+
+# Loop for the execution of the task, ideally this will be given by the tick of a BT
+for i in range(32):
+    # Set the observation from the current readings, the logic of the observations need to be specified for the task. 
+    # When an observation is unavailable set it to 'null'
+    
+    # Test
+    if i < 3:
+        obs = [1, 1, 1, 1]         # not at place location, not picked, not, reachable, not placed
+    if i >= 3 and i < 5:
+        obs = [1, 1, 0, 1]         # not at place location, not picked, reachable, not placed
+    if i>= 5 and i < 8:        
+        obs = [1, 0, 0, 1]         # not at place location, picked, reachable, not placed
+    if i>= 8 and i < 10:   # simulate losing it while carrying    
+        obs = [1, 1, 1, 1]         # not at place location, not picked, not,reachable, not placed
+    if i>= 10 and i < 12:   
+        obs = [1, 1, 0, 1]         # not at place location, not picked, reachable, not placed
+    if i>= 12 and i < 14:   
+        obs = [1, 0, 0, 1]         # not at place location, picked, reachable, not placed
+    if i>= 14 and i < 15:   
+        obs = [0, 0, 0, 1]         # at place location, picked, not placed
+    if i>= 15 and i < 20:
+        obs = [0, 0, 0, 0]         # success
+    if i>= 20 and i < 23:   # simulate losing it after placing correctly
+        obs = [1, 1, 1, 1]         # not at place location, not picked, not reachable, not placed
+    if i>= 23 and i < 25:        
+        obs = [1, 1, 0, 1]         # not at place location, not picked, reachable not placed
+    if i>= 25 and i < 28:   
+        obs = [1, 0, 0, 1]         # not at place location, picked, reachable, not placed
+    if i>= 28 and i < 30:
+        obs = [0, 0, 0, 1]         # not at place location, picked, reachable, not placed
+    if i>= 30 and i < 32:
+        obs = [0, 0, 0, 0]         # success
+
+    outcome, curr_acti = adaptive_action_selection.adapt_act_sel(ai_agent_task, obs)
+   
+    # print('Status:', outcome)
+    print('Current action(s):', curr_acti)
